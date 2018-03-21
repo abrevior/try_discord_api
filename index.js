a404091cbf5addc790fd9fc8cc036bce062a2f24
abrevior <kolesnk.s@gmail.com>
2018-03-21 22:42:14 +0200

    Remove discord bot token from reository

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const commando = require('discord.js-commando');
 const mongo = require('./mongo');
 const bot = new commando.Client();
 
-bot.registry.registerGroup('random','Random');
-bot.registry.registerGroup('task','Task');
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerGroup('task', 'Task');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
-bot.login('NDI1MzE3ODI1ODY4MjAxOTk3.DZFr-w.ez-3OMPEUc09GkILY2LFz0yINKU');
+if (!process.env.BOT_TOKEN) {
+  throw new Error('Can\'t find token for discord server login');
+}
+const token = process.env.BOT_TOKEN;
+
+async function connect() {
+  await bot.login(token);
+}
+
+connect();
+
+
